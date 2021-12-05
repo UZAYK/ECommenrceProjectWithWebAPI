e9a3a219238de6c2190ad136c9a6d3ceddcc8e51
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-05 20:24:55 +0300

    Entities Concrete de AuditableEntity oluşturuldu.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        int ICreatedEntity.CreatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        int IUpdatedEntity.CreatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        DateTime ICreatedEntity.CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        DateTime IUpdatedEntity.CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
