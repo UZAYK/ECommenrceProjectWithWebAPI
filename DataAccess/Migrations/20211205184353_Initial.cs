70fd7d7705af6505e477fe57ba959c2df300760a
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-05 21:52:17 +0300

    DataAccess'de ilk Migrations ve IBaseRepository oluşturuldu.

---
+++ DataAccess/Migrations/20211205184353_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FirsName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Gender = table.Column<bool>(type: "bit", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedUserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2021, 12, 5, 21, 43, 52, 824, DateTimeKind.Local).AddTicks(5509)),
+                    UpdatedUserId = table.Column<int>(type: "int", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "User",
+                columns: new[] { "Id", "Address", "CreatedDate", "CreatedUserId", "DateOfBirth", "Email", "FirsName", "Gender", "LastName", "Password", "UpdatedDate", "UpdatedUserId", "UserName" },
+                values: new object[] { 1, "Ankara", new DateTime(2021, 12, 5, 21, 43, 52, 849, DateTimeKind.Local).AddTicks(303), 1, new DateTime(1998, 8, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "uzay@gmail.com", "Uzay KAHRAMAN", true, "Alkın", "12345", null, null, "uzayk" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "dbo");
+        }
+    }
+}
