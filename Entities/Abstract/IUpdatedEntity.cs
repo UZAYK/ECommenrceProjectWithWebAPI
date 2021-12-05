bdeac8842fc1096a7f1066e3f64e8a0b05894a60
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-05 20:23:21 +0300

    Entities Abstract da IUpdatedEntity oluşturuldu.

---
+++ Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int CreatedUserId { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
