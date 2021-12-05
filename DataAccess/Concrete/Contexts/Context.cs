08856d775618d268baeada50e28e2aca7a136e51
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-05 21:51:25 +0300

    DataAccess'te Concrete klasöründe Context, IUserDal, EntityFramework klasöründe EfBaseRepository, Mapping ve içerisine UserMap classları eklendi.

---
+++ DataAccess/Concrete/Contexts/Context.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete.BaseEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions <Context> options) : base(options) 
+        {
+
+        }
+        public Context()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Data Source =.\\SQLEXPRESS; Initial Catalog = ECommenrceProjectWithWebAPIDb; Integrated Security = True";
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public virtual DbSet<User> Users { get; set; } 
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Mapping/UserMap.cs
@@ -0,0 +1,72 @@
+﻿using Entities.Concrete.BaseEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // @"dbo" şema
+            builder.ToTable("User", @"dbo")
+                ;
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=>x.UserName)
+                .HasColumnName("UserName")
+                .HasMaxLength(50)
+                .IsRequired(); 
+            
+            builder.Property(x=>x.FirsName)
+                .HasColumnName("FirsName")
+                .HasMaxLength(50)
+                .IsRequired(); 
+            
+            builder.Property(x=>x.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(50)
+                .IsRequired(); 
+            
+            builder.Property(x=>x.Password)
+                .HasColumnName("Password")
+                .HasMaxLength(20)
+                .IsRequired(); 
+            
+            builder.Property(x=>x.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(50)
+                .IsRequired();
+            
+            builder.Property(x=>x.Gender)
+                .HasColumnName("Gender")
+                .IsRequired();
+            
+            builder.Property(x=>x.DateOfBirth)
+                .HasColumnName("DateOfBirth")
+                .IsRequired();
+
+            builder.Property(x => x.CreatedDate).HasDefaultValue(DateTime.Now);
+
+            builder.HasData(new User
+            {
+                Id = 1,
+                UserName = "uzayk",
+                FirsName = "Uzay KAHRAMAN",
+                LastName = "Alkın",
+                Password = "12345",
+                Email = "uzay@gmail.com",
+                Gender = true,
+                DateOfBirth = Convert.ToDateTime("20-08-1998"),
+                CreatedDate = DateTime.Now,
+                Address = "Ankara",
+                CreatedUserId = 1
+            });
+        }
+    }
+}
