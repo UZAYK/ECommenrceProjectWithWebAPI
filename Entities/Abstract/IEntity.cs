d8cdbfe5502485fc3005e6c4292ef088716727e2
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-05 20:23:04 +0300

    Entities Abstract da IEntity oluşturuldu.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Abstract
+{
+    /// <summary>
+    /// Veritabanında karşılık gelen tablolarda olacak.
+    /// </summary>
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
