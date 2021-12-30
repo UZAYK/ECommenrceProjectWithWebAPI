01736dfabb573106c366556648a733dac54c844d
UzayClockwork <uzay.kahraman@clockwork.com.tr>
2021-12-31 01:09:57 +0300

    Revert "Windows Form tekrar eklendi ve Form design yapıldı. DataGrid bağlandı ve ilk apinin GetList methodu tüketildi."
    This reverts commit f4083712a820b37421529acb5c9aeea14c5747fb.

--- WebAPIWithWindowsForm/Form1.cs
+++ WebAPIWithWindowsForm/Form1.cs
@@ -1,33 +1,20 @@
-﻿using Entities.Dtos.UserDtos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WebAPIWithWindowsForm
 {
-    public partial class kullanici : Form
+    public partial class Form1 : Form
     {
-       private string url = "http://localhost:61695/api/Users/GetList";
-        public kullanici()
+        public Form1()
         {
             InitializeComponent();
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            using (HttpClient httpClient = new HttpClient())
-            {
-                var users = await httpClient.GetFromJsonAsync<List<UserDetailDto>>(new Uri(url));
-                dataGridView1.DataSource = users;
-            }
         }
     }
 }
